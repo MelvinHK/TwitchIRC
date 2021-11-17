c6bf5dc5e50ba885604a7e09da8caf33116fd02c
MelvinHK <53848567+MelvinHK@users.noreply.github.com>
2021-11-17 19:06:04 +1100

    added channel name search

--- app.js
+++ app.js
@@ -3,31 +3,43 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-var channelName = 'pokelawls';
 var content = {};
+var channelName;
+var client;
 
-const client = new tmi.Client({
-    channels: [channelName]
-});
-
-client.connect();
-client.on('message', (channel, tags, message, self) => {
-    content = {
-        message: `${tags['display-name']}: ${message}`,
-        id: `${tags.id}`
-    };
-});
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    var content = {
-        channelName: channelName
+    var currentChannel = {
+        currentChannel: channelName
     }
-    res.render('index', content);
+    res.render('index', currentChannel);
 });
 
 app.get('/messages', (req, res) => {
     res.json(content);
+});
+
+app.post('/search', (req, res) => {
+    if (client) {
+        content = {};
+        client.disconnect();
+    }
+    channelName = req.body.name;
+    client = new tmi.Client({
+        channels: [channelName]
+    });
+    client.connect();
+    client.on('message', (channel, tags, message, self) => {
+        content = {
+            message: `${tags['display-name']}: ${message}`,
+            id: `${tags.id}`
+        };
+    });
 });
 
 app.listen(port, () => console.log(`Twitch relay web app, listening at port ${port}`));
